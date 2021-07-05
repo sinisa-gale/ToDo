eed58e2f781eefb91b085a0af8dc77f90faf6a5f
sinisa-gale <gacic.sinisa@gmail.com>
2021-07-05 22:56:34 +0200

    router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
 import Clock from './components/Clock/Clock';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const Home = () => (<div><h1>Welcome home</h1></div>)
+const Homes = () => (<div><h1>Youre homes</h1></div>)
 
 function App() {
   const activities = [
@@ -25,20 +29,24 @@ function App() {
     },
   ]
 
-
-
   return (
-    <div className="notificationsFrame">
-      <div className="panel">
-        <Clock />
-        <Header title='Timeline' />
-        <Content activities={activities} />
-        <Header title='Profile' />
-        <Header />
-        <Header title='Chat' />
-        Strana 138 knjige 30 dana react
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/homes" component={Homes} />
       </div>
-    </div>
+    </Router>
+    // <div className="notificationsFrame">
+    //   <div className="panel">
+    //     <Clock />
+    //     <Header title='Timeline' />
+    //     <Content activities={activities} />
+    //     <Header title='Profile' />
+    //     <Header />
+    //     <Header title='Chat' />
+    //     Strana 153 knjige 30 dana react
+    //   </div>
+    // </div>
   );
 }
 
